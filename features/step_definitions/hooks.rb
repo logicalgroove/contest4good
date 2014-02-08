e58e54c7568761f0324f2b53c901806a78d65b88
Logicalgroove <info@logicalgroove.com>
2014-02-08 13:14:09 +0000

    First student feature tests

--- features/step_definitions/hooks.rb
+++ features/step_definitions/hooks.rb
@@ -1,4 +1,10 @@
 Before do
   DatabaseCleaner.clean
+  $browser.delete_cookie('_session', 'path=/') if $browser
+end
+
+After do
+  $browser.delete_cookie('_session', 'path=/') if $browser
+  $browser.delete_all_visible_cookies if $browser
 end
 
